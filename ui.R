7e4c56b0111f6e14576efb6f4ff27835e350df5b
Mahmoud Badr <mahmoudbadr007@gmail.com>
2020-08-21 20:11:52 +0200

    Initial commit

---
+++ ui.R
@@ -0,0 +1,39 @@
+suppressWarnings(library(shiny))
+suppressWarnings(library(markdown))
+shinyUI(navbarPage("Coursera Data Science Capstone: Course Project",
+                   tabPanel("Predict the Next Word",
+                            HTML("<strong>Author: Mahmoud Badr</strong>"),
+                            br(),
+                            HTML("<strong>Date: 21 August 2020</strong>"),
+                            br(),
+                            # Sidebar
+                            sidebarLayout(
+                                sidebarPanel(
+                                    helpText("Enter a partially complete sentence to begin the next word prediction"),
+                                    textInput("inputString", "Enter a partial sentence here",value = ""),
+                                    br(),
+                                    br(),
+                                    br(),
+                                    br()
+                                ),
+                                mainPanel(
+                                    h2("Predicted Next Word"),
+                                    verbatimTextOutput("prediction"),
+                                    strong("Sentence Input:"),
+                                    tags$style(type='text/css', '#text1 {background-color: rgba(255,255,0,0.40); color: blue;}'), 
+                                    textOutput('text1'),
+                                    br(),
+                                    strong("Note:"),
+                                    tags$style(type='text/css', '#text2 {background-color: rgba(255,255,0,0.40); color: black;}'),
+                                    textOutput('text2')
+                                )
+                            )
+                            
+                   ),
+                   tabPanel("About",
+                            mainPanel(
+                                includeMarkdown("about.md")
+                            )
+                   )
+)
+)
